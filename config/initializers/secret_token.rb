89cda499fe8cb9f22362153caaf9eddeec920d8d
sandals <sandalsboy@gmail.com>
2014-03-28 18:01:50 -0700

    Initial commit. Empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TinyPosts::Application.config.secret_key_base = 'a4ace97e4ad15d34d422182346758082d47b7e82ae296030b44e4e4e9b0fa63d745458e1a2466d8091152cde5fd18f090de63aa2d1bcca21b2213cdc2a925992'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TinyPosts::Application.config.session_store :cookie_store, key: '_tiny-posts_session'

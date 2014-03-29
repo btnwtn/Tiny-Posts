f261333bf86b78048ac18387a9aee301c2db9150
sandals <sandalsboy@gmail.com>
2014-03-28 20:09:43 -0700

    Added the "create" functionality to the Post controller. The creation form has been prettified as well.

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,28 @@
 class PostController < ApplicationController
+	# GET /posts
 	def index
 		@posts = Post.order("id desc")
 	end
+
+	# GET /post/new
+	def new
+		@post = Post.new
+	end
+
+	# POST /post
+	def create
+		@post = Post.new(post_parameters)
+
+		if @post.save
+			redirect_to post_index_path
+		else
+			render :new
+		end
+	end
+
+	private
+		# only allow content and author data to be submitted.
+		def post_parameters
+			params.require(:post).permit(:content, :author)
+		end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	# each post must have an author and some content
+	validates_presence_of :author, :content
+	# content may not be longer than 140 characters in length
+	validates :content, length: { maximum: 140 }
 end

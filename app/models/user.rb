bec713ccbe164f40a51fe23b1589ff82feb7628f
sandals <sandalsboy@gmail.com>
2014-03-31 22:10:22 -0700

    Make users posts dependent on the user existing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	has_many :posts
+  # if a user is deleted, delete all their posts
+	has_many :posts, dependent: :destroy
 
 	validates_presence_of :password, :on => :create
 	validates_presence_of :email
